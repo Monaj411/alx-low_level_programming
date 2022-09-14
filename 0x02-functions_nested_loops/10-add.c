01f69dc8423fc5ce3aec4b63347990997e4478af
Monaj411 <mohammednajeem18@gmail.com>
2022-09-13 17:17:54 -0700

    loop

--- 0x02-functions_nested_loops/10-add.c
+++ 0x02-functions_nested_loops/10-add.c
@@ -4,7 +4,8 @@
 * @i: The first number to be added.
 * @j: The second number to be added.
 * Return: the sum of two numbers.
-*/int add(int i, int j)
+*/
+int add(int i, int j)
 {
 return (i + j);
 }
